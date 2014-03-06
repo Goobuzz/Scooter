550074ddc8030552f0d3f86445ff3ea84818c119
Raymond Hulha <raymond@gootechnologies.com>
2014-03-06 18:04:51 +0100

    new Goo Engine and bundle

--- js/loadScene.js
+++ js/loadScene.js
@@ -1,19 +1,13 @@
 require([
-    'goo/entities/GooRunner',
-    'goo/statemachine/FSMSystem',
-    'goo/addons/howler/systems/HowlerSystem',
-    'goo/loaders/DynamicLoader',
-	'goo/entities/components/ScriptComponent',
-    'goo/addons/ammo/AmmoSystem',
-    'goo/addons/ammo/AmmoComponent',
+	'goo/entities/GooRunner',
+	'goo/loaders/DynamicLoader',
+	'goo/addons/ammo/AmmoSystem',
+	'goo/addons/ammo/AmmoComponent',
     'goo/math/Vector3',
     'js/VehicleHelper'
 ], function (
-    GooRunner,
-    FSMSystem,
-    HowlerSystem,
-    DynamicLoader,
-    ScriptComponent,
+	GooRunner,
+	DynamicLoader,
     AmmoSystem,
     AmmoComponent,
     Vector3,
@@ -22,12 +16,6 @@ require([
 	'use strict';
 
 	function init() {
-
-		if (window.location.protocol==='file:') {
-			alert('You need to run this webpage on a server. Check the code for links and details.');
-			return;
-		}
-
 		// Make sure user is running Chrome/Firefox and that a WebGL context works
 		var isChrome, isFirefox, isIE, isOpera, isSafari, isCocoonJS;
 		isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
@@ -66,77 +54,109 @@ require([
 				antialias: true,
 				manuallyStartGameLoop: true
 			});
-			var fsm = new FSMSystem(goo);
-			goo.world.setSystem(fsm);
-			goo.world.setSystem(new HowlerSystem());
-			
+
 			var ammoSystem = new AmmoSystem();
 			goo.world.setSystem(ammoSystem);
+			
 
 			// The loader takes care of loading the data
 			var loader = new DynamicLoader({
 				world: goo.world,
 				rootPath: 'res',
-				progressCallback: progressCallback});
+				progressCallback: progressCallback
+			});
 
-			loader.loadFromBundle('project.project', 'root.bundle', {recursive: false, preloadBinaries: true}).then(function(configs) {
+			loader.load('root.bundle').then(function(result) {
+
+				// Grab the first project in the bundle.
+				var bundleKeys = Object.keys(result);
+				var projectIds = bundleKeys.filter(function(k) {
+					return /\.project$/.test(k);
+				});
+				var projectId = projectIds[0];
+				if (!projectId) {
+					alert('Error: No project in bundle'); // Should never happen
+					return null;
+				}
+				
+				return loader.load(projectId);
+
+			}).then(function(obj) {
 
 				// This code will be called when the project has finished loading.
 				goo.renderer.domElement.id = 'goo';
 				document.body.appendChild(goo.renderer.domElement);
 
 				// Application code goes here!
-				
+
+				// Process the world, so all entity world bounds and worldTransforms are up to date.
 				goo.world.process();
-
-				loader.getCachedObjectForRef('car/entities/prop_mesh_0.entity')
-					.setComponent(new ScriptComponent({ run: function (entity) {
-						entity.transformComponent.setRotation( 0, goo.world.time * 20, 0);
+				var es = obj.mainScene.entities;
+				// Get all the duplicated star entities and give them real physical behavior using the AmmoComponent
+				for( var k in es ) {
+					console.log( es[k].name);
+					if(es[k].name == 'Star') {
+						es[k].setTag('Star');
+						es[k].setComponent(new AmmoComponent({mass:3, useWorldBounds:true, showBounds:false}));
 					}
-				}));
-				
-				for( var k in configs)if(k[k.length-1] == 'y' ) {
-					console.log(k);
-					if( k.indexOf('star_plain/entities/RootNode') < 0 )
-						continue;
-					var star = loader.getCachedObjectForRef( k );
-					star.setComponent(new AmmoComponent({mass:3, useWorldBounds:true, showBounds:false}));
 				}
-
-				var logo = loader.getCachedObjectForRef('goo_logo/entities/goo_logo_mesh_0.entity');
+				var em = goo.world.entityManager;
+				var car  = em.getEntityByName('Car');
+				var logo = em.getEntityByName('goo_logo_mesh'); // 'Goo Logo'
+				var prop = em.getEntityByName('prop_mesh')
+				var cam  = em.getEntityByName('Default Camera')
+				// make the logo collidable using the AmmoComponent.
+				// AmmoComponents with a mass=0 are static and will not move.
 				logo.setComponent(new AmmoComponent({mass:0, useWorldTransform:true}));
-
-				var car = loader.getCachedObjectForRef('car/entities/RootNode.entity');
-				var pos = car.transformComponent.transform.translation.clone();
+				// Lift the car a bit up, so it won't stick inside the logo. ( Remove this and see what happens )
+				car.addTranslation(0,1,0);
+				// Copy the car position so we can reset it, when we fall of the track
+				var pos = car.getTranslation().clone();
+				// You can think of the mass number as being in kilos.
 				car.setComponent(new AmmoComponent({mass:350, useWorldBounds:true, showBounds:false}));
+				// We use the helper class to make our car behave like a real car
+				// It makes life easier when dealing with AmmoJS internal RayCastVehicle
+				// The parameters are: goo, ammoSystem, chassis, wheelRadius, suspensionLength, doCreateDebugTire
 				var vehicleHelper = new VehicleHelper(goo, ammoSystem, car, 2, 0.6, false);
+				// The wheel axle is a vector that describes the orientation of the axle
+				// Typical choices are the the x (1,0,0) axis or the z (0,0,1) axis.
+				// Try setWheelAxle( -1, 0, 0); and see what happens.
 				vehicleHelper.setWheelAxle( 1, 0, 0);
+				// We add 4 wheels at the corners of the entity bounding box
+				// If you want to be more precise you will have to position each wheel manually.
+				// Look at the helper class to see how it is done.
 				vehicleHelper.addDefaultWheels();
-
-				var cam = loader.getCachedObjectForRef('entities/ToolCamera.entity');
-				cam.scriptComponent.scripts = [];
-				//car.transformComponent.attachChild( cam.transformComponent);
-				//cam.transformComponent.setTranslation(0,10,-10);
-				//cam.transformComponent.transform.rotation.lookAt( new Vector3(0,1,-1), new Vector3(0,1,0));
-				
+				//set a ScriptComponent that rotates the propeller around the UP axis.
+				prop.set( function (entity, tpf) {
+					entity.transformComponent.addRotation( 0, tpf*5, 0);
+				});
+				/*	The following code creates a script that makes the camera follow the car.
+					First we create an aboveCar vector object that is set to a position 1 unit above the car
+					and then a behindCar vector object that is set to a position 3 units behind
+					and 2.5 units above the car.
+					We then use the behindCar vector to linearly interpolate (lerp) the camera to that position
+					Finally we make the camera lookAt at the aboveCar position.
+					applyPost is a cool function of the rotation object (Matrix3x3) which applies it's rotation
+					to the passed in vector object and because we want the vector to point behind the car
+					we pass in a vector with a negative z value.
+					You can think of it like putting the vector into the a local coordinate system of the car.
+				*/
 				var aboveCar = new Vector3();
 				var behindCar = new Vector3();
 				var camScriptObject = {};
 				camScriptObject.run = function(entity,tpf) {
 					var transform = car.transformComponent.transform;
 					var pos = transform.translation;
-
 					behindCar.setd(0,0,-3);
 					transform.rotation.applyPost(behindCar);
 					behindCar.addv(pos).add_d(0,2.5,0);
-
 					entity.transformComponent.transform.translation.lerp(behindCar,0.05);
 					entity.lookAt(aboveCar.setv(pos).add_d(0,1,0),Vector3.UNIT_Y);
 				}
 				
-				cam.scriptComponent.scripts.push(camScriptObject);
-
-
+				// We replace the default Orbit camera behavior with our own from above
+				cam.scriptComponent.scripts = [camScriptObject];
+				// This is a code block I love to use to collect key presses
 				var keys = new Array(127).join('0').split('').map(parseFloat); // prefill with 0s
 				var keyHandler = function (e) {
 					keys[e.keyCode] = e.type === "keydown" ? 1 : 0;
@@ -146,19 +166,23 @@ require([
 				}
 				document.body.addEventListener('keyup', keyHandler, false);
 				document.body.addEventListener('keydown', keyHandler, false);
+				// Finally we add a goo callback function. This makes the last 3 lines of code being called every frame.
+				// So every frame we check if one of the cursor keys is pressed and if so we call the
+				// setSteeringValue or the applyEngineForce functions with values > 0.
+				// This effectively accelerates and steers the car.
+				// ( There is also a setBrake function available if you need that. )
 				goo.callbacks.push(function() {
 					vehicleHelper.setSteeringValue( keys[37] * 0.3 + keys[39] * -0.3);
 					vehicleHelper.applyEngineForce( keys[38] * -700 + keys[40] * 500);
 					vehicleHelper.applyEngineForce( keys[38] * -700 + keys[40] * 500, false);
 				});
 
-
 				// Start the rendering loop!
 				goo.startGameLoop();
 
 			}).then(null, function(e) {
 				// If something goes wrong, 'e' is the error message from the engine.
-				console.log('Failed to load scene: ' + e.stack);
+				alert('Failed to load project: ' + e);
 			});
 
 		}
